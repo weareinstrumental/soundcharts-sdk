f05db5f98656ef4c403d3db95a9e209cdd0782f8
Simon Christian <simon.christian@weareinstrumental.com>
2021-06-24 20:58:54 +0100

    Added other social platforms

--- src/soundcharts/platform.py
+++ src/soundcharts/platform.py
@@ -3,9 +3,14 @@ from enum import Enum
 
 class SocialPlatform(Enum):
 
-    INSTAGRAM = "instagram"
-    SPOTIFY = "spotify"
-    TWITTER = "twitter"
+    AMAZON = "amazon"
     APPLE_MUSIC = "apple-music"
     DEEZER = "deezer"
+    FACEBOOK = "facebook"
+    INSTAGRAM = "instagram"
     ITUNES = "itunes"
+    SHAZAM = "shazam"
+    SPOTIFY = "spotify"
+    TIKTOK = "tiktok"
+    TWITTER = "twitter"
+    YOUTUBE = "youtube"
