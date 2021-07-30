02d2d6cebbc5b25082fc6810c45bc8970c0f9bd9
Simon Christian <simon.christian@weareinstrumental.com>
2021-07-30 12:51:04 +0100

    Update version to 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="soundcharts-sdk",
-    version="1.2.1",
+    version="1.3.0",
     description="SDK for Soundcharts API",
     author="Simon Christian",
     author_email="simon.christian@weareinstrumental.com",
