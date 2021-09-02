dbd0a94a588c073ff8a2593a8956cca5895714f4
Simon Christian <simon.christian@weareinstrumental.com>
2021-09-02 12:05:37 +0100

    Update version to 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="soundcharts-sdk",
-    version="1.3.3",
+    version="1.4.0",
     description="SDK for Soundcharts API",
     author="Simon Christian",
     author_email="simon.christian@weareinstrumental.com",
