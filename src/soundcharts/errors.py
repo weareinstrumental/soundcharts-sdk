eb786d2ebdf420eb860e6496633266cdf30c13ce
Simon Christian <sjchristian@gmail.com>
2022-06-14 11:34:44 +0100

    Update Soundcharts client to support error format better
    This isn't perfect but should expose the error messages fully instead of hiding the detail

--- src/soundcharts/errors.py
+++ src/soundcharts/errors.py
@@ -1,9 +1,16 @@
+from black import err
+
+
 class Error(Exception):
     pass
 
 
 class ConnectionError(Error):
-    pass
+    def __init__(self, url, status_code, errors: list = None):
+        self.url = url
+        self.status_code = status_code
+        self.errors = errors
+        super().__init__(f"Exception from endpoint {self.url}: {[e['message'] for e in errors]}")
 
 
 class IncorrectReponseType(Error):
