b07efb3ebc255202d72fb968314678a00fc5ada2
Simon Christian <sjchristian@gmail.com>
2022-08-09 16:44:20 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="SDK for Soundcharts API",
     author="Simon Christian",
     author_email="simon.christian@weareinstrumental.com",
-    url="https://bitbucket.org/weareinstrumental/soundcharts-sdk",
+    url="https://github.com/weareinstrumental/soundcharts-sdk",
     packages=find_packages("src", exclude=["tests"]),
     package_dir={"": "src"},
     include_package_data=True,
